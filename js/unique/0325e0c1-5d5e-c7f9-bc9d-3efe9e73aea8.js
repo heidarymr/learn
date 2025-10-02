d7f1e869188b27111254279f675187d466433ff7
MRH <heidarymr@gmail.com>
2025-10-02 20:09:16 +0000

    Move all JS module-specific logic to unique folder for modular structure

---
+++ js/unique/0325e0c1-5d5e-c7f9-bc9d-3efe9e73aea8.js
@@ -0,0 +1,9 @@
+// اختصاصی برای 4000 Essential English Words 1
+var path = "/apps-data/4000-essential-english-words-1/";
+var lsession = "lastestSession-" + path.split("/")[2];
+var lsetting = "settings-" + path.split("/")[2];
+var lfontSetting = "fontSize-" + path.split("/")[2];
+var loadedTime = 0;
+var ads = false;
+var countUnitLearned = 0;
+// سایر توابع و منطق اختصاصی این مجموعه را اینجا قرار دهید
---
+++ js/unique/8200a76e-2b00-b6d8-0cc1-94a620626b9b.js
@@ -0,0 +1,9 @@
+// اختصاصی برای 250 Ways to Say it in Business English
+var path = "/apps-data/250-ways-to-say-it-in-business-english/";
+var lsession = "lastestSession-" + path.split("/")[2];
+var lsetting = "settings-" + path.split("/")[2];
+var lfontSetting = "fontSize-" + path.split("/")[2];
+var loadedTime = 0;
+var ads = false;
+var countUnitLearned = 0;
+// سایر توابع و منطق اختصاصی این مجموعه را اینجا قرار دهید
